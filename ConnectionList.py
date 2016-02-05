7ea3c4bfe505bef92a08e7f53f062effaddbcfc4
Mikael Blomstrand <mbloms@kth.se>
2016-02-05 20:27:53 +0100

    Added support for reading accesses from text file. Not testet yet.

--- ConnectionList.py
+++ ConnectionList.py
@@ -1,16 +1,31 @@
+# Using Twithon, install with sudo pip3 install twython
+from twython import Twython
+
 class ConnectionList:
 	"List of (Twython) objects that rotates every time a 'connection' is extracted."
-	def __init__(self,arr):
+	def __init__(self,arr = [], filepath = ""):
+		if filepath == "":
+			arr = readFile()
 		#The array with all objects
 		self.arr = arr
 		#Start index
 		self.index = 0
-		#Size of the array
-		self.size = len(arr)
 
 	def connection(self):
 		"Rotates the index value and returns the object at that index"
 		self.__rotate()
 		return self.arr[self.index]
 	def __rotate(self):
-		self.index = (self.index + 1) % self.size
+		self.index = (self.index + 1) % len(self.arr)
+
+def readFile():
+	lst = []
+	txt = open("accesses.txt", 'r')
+	#Read first line
+	line = txt.readline()
+	#While there is more
+	while line != "":
+		#Make Twython objects of four lines. If the lines in the file is not devisable by 4, something will go bad.
+		lst.append(Twython(line,txt.readline(),txt.readline(),txt.readline()))
+		line = txt.readline()
+	return lst
