f05cdfca074189e7b0f9fb05f6e8e9062c8545e6
Andreas Brommund <andreas@brommund.se>
2016-02-05 18:37:19 +0100

    Fix the get_all_followers so the program sleeps for 16 minutes if you get a TwythonRateLimitError.

--- twython_example.py
+++ twython_example.py
@@ -4,29 +4,41 @@ import json
 import time
 
 def main():
-    APP_KEY = 'PcVBQSI6dxa6FeVP8uuanEK0V'
-    APP_SECRET = 'jt03KeTK1yivxfKXzK9CDxyKj7tGrd5VIcUF846dO0jwxBuqW8'
-    OAUTH_TOKEN = "3221674156-AffFW5MZDq9NVcEPB7gIuqDsj5xDHZjNnoiunsE"
-    OAUTH_TOKEN_SECRET = "Nl4SOOWr2aqeLgX9CrkeTRIrkfNO6vIsLBwoe3J6ozp6m"
+    
+    APP_KEY = 'Gqw3LVScCY4rZ3wfA1Gbe8FIN'
+    APP_SECRET = 'HYuHv5h6OlUqmKp9D5yfX0hNnNWlvrSFIq05Mq1hUE2E1Y6Enh'
+    OAUTH_TOKEN = "4877768477-iygFnDTiDCTQG8Gve9s2tKc0GNuTvApHkOYxbaz"
+    OAUTH_TOKEN_SECRET = "l1rCfP1lZSiHK6I82PZRhIJdSjIujD2w0S64LXZNlhpwE"
 
     twitter = Twython(APP_KEY, APP_SECRET,OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     f = ["AndreasBrommund","HadarGreinsmark","Borslampan","dprintz3","benlandis",
          "ugglapuggla","anderseriksson","Thenilsdc","DavidSkeppstedt","JacobOljemark",
-         "S0fiaPetterss0n","SweClockers","AlvinRisk","TonyKungsholmen","MrMadhawk",
+         "S0fiaPetterss0n","SweClockers","AlvinRisk","TonyKungsholmen","Aftonbladet","MrMadhawk",
          "R_Fredriksson"]
     
-    get_all_followers(twitter,f[14])
-
-
+    get_all_followers(twitter,f[14]) #14
+    
 
 def get_all_followers(twitter,user):
     cursor = -1 #default cursor
         
     while cursor != 0:
-        response = twitter.get_followers_ids(screen_name = str(user),cursor = cursor)
-        for follower_id in response['ids']:
-            print(follower_id)
-        cursor = response['next_cursor']
+        
+        twitter = connection
+        current_cursor = cursor
+
+        try:
+            response = twitter.get_followers_ids(screen_name = str(user),cursor = cursor)
+
+            for follower_id in response['ids']:
+                print(follower_id)
+                cursor = response['next_cursor']
 
 
+        except TwythonRateLimitError:
+            time.sleep(60*15+60) #In sec. 60*15 = 15 min + 1min
+            cursor = current_cursor
+
+
+    
 main()
