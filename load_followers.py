a1d53669ca62ee4c0e26502312f79f538e3499b0
Andreas Brommund <andreas@brommund.se>
2016-02-06 12:22:11 +0100

    Get alla followers from the parties in the list

--- load_followers.py
+++ load_followers.py
@@ -2,24 +2,30 @@
 from twython import Twython,TwythonRateLimitError
 import json
 import time
+import ConnectionList as cl
 
 def main():
     
-    APP_KEY = 'Gqw3LVScCY4rZ3wfA1Gbe8FIN'
-    APP_SECRET = 'HYuHv5h6OlUqmKp9D5yfX0hNnNWlvrSFIq05Mq1hUE2E1Y6Enh'
-    OAUTH_TOKEN = "4877768477-iygFnDTiDCTQG8Gve9s2tKc0GNuTvApHkOYxbaz"
-    OAUTH_TOKEN_SECRET = "l1rCfP1lZSiHK6I82PZRhIJdSjIujD2w0S64LXZNlhpwE"
-
-    twitter = Twython(APP_KEY, APP_SECRET,OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    f = ["AndreasBrommund","HadarGreinsmark","Borslampan","dprintz3","benlandis",
-         "ugglapuggla","anderseriksson","Thenilsdc","DavidSkeppstedt","JacobOljemark",
-         "S0fiaPetterss0n","SweClockers","AlvinRisk","TonyKungsholmen","Aftonbladet","MrMadhawk",
-         "R_Fredriksson"]
+    f = ["socialdemokrat",
+         "vansterpartiet",
+         "miljopartiet",
+         "sdriks",
+         "nya_moderaterna",
+         "liberalerna",
+         "kdriks",
+         "Centerpartiet"]
     
-    get_all_followers(twitter,f[14]) #14
-    
+    get_users_followers(f)    
 
-def get_all_followers(conn,user):
+
+def get_users_followers(users):
+    for u in users:
+        get_followers(u)
+
+
+def get_followers(user):
+    conn = cl.ConnectionList([],"config/access.conf") 
+
     cursor = -1 #default cursor
         
     while cursor != 0:
@@ -27,7 +33,7 @@ def get_all_followers(conn,user):
         current_cursor = cursor
 
         try:
-            response = conn.get_followers_ids(screen_name = str(user),cursor = cursor)
+            response = conn.connection().get_followers_ids(screen_name = str(user),cursor = cursor)
 
             for follower_id in response['ids']:
                 print(follower_id)
