62d7262154868d6cd1b717a5fd554b4acf5ee0d3
David Skeppstedt <david.skeppstedt@gmail.com>
2016-02-07 14:41:27 +0100

    Better twython code. Only one mehtod to create a instance of an Application-only authentication twython object.

--- tw_oauth.py
+++ tw_oauth.py
@@ -1,10 +1,9 @@
 from twython import Twython
 
-def generateAccessToken(app_key,app_secret):
-    """Generates access token for a twhython object."""
+def newTwython(app_key,app_secret):
+    """Creates a new instance of a twython object. Authorized with 
+    Application-only authentication oauth2"""
     twitter = Twython(app_key,app_secret,oauth_version=2)
-    return twitter.obtain_access_token()
+    twitter_access = Twython(app_key,access_token=twitter.obtain_access_token())
+    return twitter_access 
 
-def newTwython(app_key,access_token):
-    """Creates a instance of an Twhython object authed with a access token"""
-    return Twython(app_key,access_token=access_token)
