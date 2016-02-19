1a3045d09059f3aca8c6d79738447af0de284165
Johan Myrsmeden <myrsmeden@Johans-MacBook-Pro.local>
2016-02-19 18:04:31 +0100

    Adding new file NightlyService to hold the frame for our nightly service

---
+++ NightlyService.py
@@ -0,0 +1,20 @@
+from twython import TwythonRateLimitError,TwythonError
+import time
+import ConnectionList as CL
+import LoadFollowers as LF
+import Database
+import datetime
+
+class NightlyService:
+	""" 
+		The frame for our Nightly service with these main functions:
+		• Update list of followers to our groups
+		• Fetch tweets for new users
+		• Update tweets for already existing users
+	"""
+	def __init__(self):
+		pass
+
+	def updateFollowers(self):
+		loadFollowers = FL()
+		loadFollowers.getUsersFollowers()
