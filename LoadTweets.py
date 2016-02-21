4318d8f3b8ebc234d3e07b96ca29566ac8784a91
Johan Myrsmeden <myrsmeden@n133-p214.kthopen.kth.se>
2016-02-21 13:37:51 +0100

    Putting functions in LoadTweets into class

--- LoadTweets.py
+++ LoadTweets.py
@@ -14,77 +14,82 @@ class Tweet:
         self.content = content
         self.hashtags = hashtags
 
-def jsonToTweet(userId,tweet):
-    hashtags = []
-    for hashtag in (tweet['entities']['hashtags']):
-        hashtags.append(hashtag['text'])
+class LoadTweets:
+    def __init__(self):
+        self.db = Database.Database()
+        self.conn = CL.ConnectionList(filepath="config/access.conf") 
 
-    id = tweet['id']
-    timestamp = tweet['created_at']
-    content = tweet['text']
+    def jsonToTweet(self, userId,tweet):
+        hashtags = []
+        for hashtag in (tweet['entities']['hashtags']):
+            hashtags.append(hashtag['text'])
 
-    return Tweet(id,userId,timestamp,content,hashtags)
+        id = tweet['id']
+        timestamp = tweet['created_at']
+        content = tweet['text']
 
-def getLastTweetId(userid,db):
-    """Returns the latest twitter id,if the user does not exists or have not tweeted we return None"""
-    db.cursor.execute("SELECT coalesce(tweetid,-1)AS tweetid FROM usr" +
-                                         " NATURAL LEFT JOIN tweet WHERE userid = %s ORDER BY TIMESTAMP DESC LIMIT 1",(userid,))
-    id = db.cursor.fetchone()
-    if id == None:
-        return None
-    return id[0] if id[0] != -1 else None
+        return Tweet(id,userId,timestamp,content,hashtags)
 
-
-def getTweets(userId,db,conn,sinceId=None):
-
-    maxId = None
-
-
-    tweets = []
-
-    while True:
-
-        
-        try:
-            response = conn.connection().get_user_timeline(user_id = userId,count=200,include_rts = False, trim_user = True, max_id = maxId, since_id = sinceId)
-
-            if response == []:
-                break
-
-            for jsontw in response:
-                data = jsonToTweet(userId,jsontw)
-                print(data)
-                try:
-                    db.cursor.execute("INSERT INTO tweet(tweetId,userId,timestamp,content) VALUES (%s,%s,%s,%s)",(data.id,data.userId,data.timestamp,data.content,))
-                except: 
-                    pass
-                finally:
-                    db.commit()
+    def getLastTweetId(self, userid):
+        """Returns the latest twitter id,if the user does not exists or have not tweeted we return None"""
+        self.db.cursor.execute("SELECT coalesce(tweetid,-1)AS tweetid FROM usr" +
+                                             " NATURAL LEFT JOIN tweet WHERE userid = %s ORDER BY TIMESTAMP DESC LIMIT 1",(userid,))
+        id = self.db.cursor.fetchone()
+        if id == None:
+            return None
+        return id[0] if id[0] != -1 else None
 
 
-                try:
-                    for tag in data.hashtags:
-                        db.cursor.execute("INSERT INTO tweettag(tweetId,tag) VALUES (%s,%s)",(data.id,tag,))
-                except: 
-                    pass
-                finally:
-                    db.commit()
+    def getTweets(self,userId,sinceId=None):
+
+        maxId = None
 
 
-                maxId = data.id-1
-    
-                        
-        except TwythonAuthError:
-            print("Private account")
-            #Add to blacklist
-            break
+        tweets = []
+
+        while True:
+
             
-        except TwythonRateLimitError as err:
-            print(":(")
-            print(err)
-            print(datetime.datetime.now())
-            time.sleep(60*15+60) #In sec. 60*15 = 15 min + 1min 
-            print(":)")  
-        except TwythonError as err: #Handel timeouts
-            print("Error:")
-            print(err)
+            try:
+                response = self.conn.connection().get_user_timeline(user_id = userId,count=200,include_rts = False, trim_user = True, max_id = maxId, since_id = sinceId)
+
+                if response == []:
+                    break
+
+                for jsontw in response:
+                    data = self.jsonToTweet(userId,jsontw)
+                    print(data)
+                    try:
+                        self.db.cursor.execute("INSERT INTO tweet(tweetId,userId,timestamp,content) VALUES (%s,%s,%s,%s)",(data.id,data.userId,data.timestamp,data.content,))
+                    except: 
+                        pass
+                    finally:
+                        self.db.commit()
+
+
+                    try:
+                        for tag in data.hashtags:
+                            self.db.cursor.execute("INSERT INTO tweettag(tweetId,tag) VALUES (%s,%s)",(data.id,tag,))
+                    except: 
+                        pass
+                    finally:
+                        self.db.commit()
+
+
+                    maxId = data.id-1
+        
+                            
+            except TwythonAuthError:
+                print("Private account")
+                #Add to blacklist
+                break
+                
+            except TwythonRateLimitError as err:
+                print(":(")
+                print(err)
+                print(datetime.datetime.now())
+                time.sleep(60*15+60) #In sec. 60*15 = 15 min + 1min 
+                print(":)")  
+            except TwythonError as err: #Handel timeouts
+                print("Error:")
+                print(err)
