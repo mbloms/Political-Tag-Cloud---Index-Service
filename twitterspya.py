919185156464e6ee8c6ddcc8c7f71f1af86697de
Mikael Blomstrand <mbloms@kth.se>
2016-03-30 22:52:00 +0200

    Bättre dokumenterad kod i twitterspya. Koden läser userids från stdin och skriver tweets till stdout. logg skrivs till stderr. Alternativ för att bara ta vissa userids finns, men en snyggare lösning behövs.

--- twitterspya.py
+++ twitterspya.py
@@ -1,5 +1,7 @@
 from requests.packages.urllib3 import response
 
+#Jag gjorde ett försök med mys options och skit. Funkade inte. Importer behöver rensas.
+
 import ConnectionList as CL
 import json
 from twython import Twython,TwythonRateLimitError,TwythonError,TwythonAuthError
@@ -9,53 +11,35 @@ import datetime
 import time
 import io
 
-inputfile = sys.stdin
-outputfile = sys.stdout
 
-def readUserId(flags):
-    if not ('-m' in flags and '-r' in flags):
-        return readHelper(flags)
-    userId = inputfile.readline()
-    while (userId % flags['-m'] != flags['-r']) or (userId < flags['-s']):
-        userId = inputfile.readline()
+def readUserId():
+    '''Läser argumenten från terminalen. "% a b" kommer bara behandla de userIds där (userId % a == b) dvs userId delat med a ska ge rest b.
+    Om det inte getts några argument läser den bara allt, men strippar strängarna på whitespace.'''
+    if not len(sys.argv)>1:
+        return input()
+    userId = input()
+    while (userId % sys.argv[1] != sys.argv[2]):
+        userId = input()
     return userId.strip()
 
-def badUserId(flags, userId):
-    '''mer för läsbarhet än något annat, används inte just nu...'''
-    return (userId % flags['-m'] != flags['-r']) or (userId < flags['-s'])
-
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv,"s:m:r:i:o:d:T")
-    except getopt.GetoptError:
-        print('-s <minimum userId> -m <modulo> -r <rest>; -i <inputfile> -o <outputfile> -d <dump hashtags to file> -T (Trunctate file)', file=sys.stderr) #(-w <väntetid vid limit>) -t <försök vid limit>
-    flags = {}
-    for opt, arg in opts:
-        flags[opt] = arg
+    #variabel som används för att kolla hur många connections som testats innan skriptet sover.
     timeout = 1
     conn = CL.ConnectionList(filepath="config/access.conf")
-    if '-t' in flags:
-        TRIES = flags['-t']
-    else:
-        TRIES = conn.size()
-    if '-i' in flags:
-        inputfile=open(flags['-i'])
-    if '-T' in flags:
-        writeMode = 'w'
-    else:
-        writeMode = 'a'
-    if '-o' in flags:
-        outputfile=open(flags['-o'], 'a')
-    print(flags)
     maxId = None
-    userId = readUserId(flags)
+    #Läs första idt från stdin.
+    userId = readUserId()
+    #Variabel som räknar antalet nedladdade tweets.
     printcount = 0
+    #variabler för att skriva ut tidsstämplar.
     first = 0
     start = time.time()
     while True:
         try:
+            #Hämta nästa respons
             response = conn.connection().get_user_timeline(user_id = userId,count=200,include_rts = True, trim_user = True, max_id = maxId)
             while response == []:
+                #Om tom respons skriv ut info till loggen (stderr)
                 end = time.time()
                 last = printcount
                 print("User Id: " +'\t'+ str(userId.strip())+'\n', file=sys.stderr)
@@ -65,32 +49,32 @@ def main():
                 print("Timestamp: " +'\t'+ str(datetime.datetime.now()), file=sys.stderr)
                 start = time.time()
                 first = printcount
-                userId = readUserId(flags)
+                #Läs nästa id, nollställ maxId och hämta ny respons.
+                userId = readUserId()
                 maxId = None
                 response = conn.connection().get_user_timeline(user_id = userId,count=200,include_rts = True, trim_user = True, max_id = maxId)
 
             for stuff in response:
-                print(json.dumps(stuff), file=outputfile)
-                if '-d' in flags:
-                    dumpfile = open(flags['-d'], 'a')
-                    for hashtag in stuff['entities']['hashtags']
-                        print(hashtag, file=dumpfile)
-                    dumpfile.close()
+                #skriv ut tweeten (och allt annat stuff som hör till) som ren json som man fick den från twitter. 
+                print(json.dumps(stuff))
+                #incrementera antalet nedladdade tweets.
                 printcount += 1
 
-
+            #Hämta bara tweets som ligger efter de sista tweetsen.
             maxId = response[-1]['id']-1
 
         except TwythonAuthError:
+                    #Skriv ut dummy json vid privat konto. på det sättet får inte en jsonparser spasmer, men vi har fortfarande info om de kontona.
                     print('{"text": "privateaccount", "entities": {"symbols": [], "urls": [], "hashtags": [], "user_mentions": []}, "id": null, "user": {"id_str": "'+str(userId)+'", "id": '+str(userId)+'}, "created_at": null, "is_quote_status": false, "in_reply_to_user_id_str": null, "id_str": null, "lang": null, "place": null, "in_reply_to_user_id": null, "source": null, "in_reply_to_status_id_str": null}')
-                    userId = readUserId(flags)
+                    #Läs nästa
+                    userId = readUserId()
                     maxId = None
                     
                     
         except TwythonRateLimitError as err:
             timeout += 1
             print("access "+str(conn.position())+" timed out.", file=sys.stderr)
-            if timeout > TRIES:
+            if timeout > conn.size():
                 timeout = 1
                 print(":(", file=sys.stderr)
                 print(err, file=sys.stderr)
@@ -104,11 +88,14 @@ def main():
             print(eof, file=sys.stderr)
             break
         except Exception as other:
+            #Vid annat felmeddelande skriv ut felmeddelandet som json så att det går att se i efterhand.
+            #Meddelandet skrivs ut på en rad genom att alla newlines byts ut till '\\n'
             print(other, file=sys.stderr)
             print("{\"userId\":"+str(userId)+",\"maxId\":"+str(maxId)+" \"error\":\"")
             print(str(other).replace('\n',"\\n"))
             print("\"}")
-            userId = readUserId(modMode)
+            #Läs nästa
+            userId = readUserId()
             maxId = None
 
 main()
