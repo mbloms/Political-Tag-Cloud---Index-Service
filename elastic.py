4fe3ae9cfb38ae81b2ff0c7da24cead79f0aa729
David Skeppstedt <david.skeppstedt@gmail.com>
2016-04-12 10:32:28 +0200

    Mer refaktorering. Lade till lite class variabler for index och doc_type. La också till en method som heter checkImport.

--- elastic.py
+++ elastic.py
@@ -7,22 +7,27 @@ class ElasticImporter:
 	def __init__(self,path):
 		self.es = Elasticsearch()
 		self.path = path
+		self.index = "Tweets"
+		self.doc_type = "tweet"
 
 	def importTweets(self):
-
-		with open("test100k.json", "r") as data_file:
+		with open(self.path, "r") as data_file:
 			for line in data_file:
 				doc = json.loads(line)
 				user_id = doc["user_id"]
-				res = es.index(index="test-index", doc_type='tweet', body=doc)
+
+				res = es.index(index=self.index,
+					doc_type=self.doc_type, body=doc)
+
 				print(res['created'])
 
-		es.indices.refresh(index="test-index")
-
-		res = es.search(index="test-index", body={"query": {"match_all": {}}})
+	def checkImport(self):
+		es.indices.refresh(index=self.index)
+		res = es.search(index=self.index, body={"query": {"match_all": {}}})
 		print("Got %d Hits:" % res['hits']['total'])
 		for hit in res['hits']['hits']:
-		    print("%(user_id)s %(tweet_id)s: %(text)s" % hit["_source"])
+			print("%(user_id)s %(tweet_id)s: %(text)s" % hit["_source"])
+
 def main(args):
 	print(args)
 
