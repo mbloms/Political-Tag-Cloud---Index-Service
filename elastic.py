68e90be12914b1cc869bc1a442d9420e66a6b028
David Skeppstedt <david.skeppstedt@gmail.com>
2016-04-12 11:05:47 +0200

    Gjorde några sista tweaks och nu funkar det... typ

--- elastic.py
+++ elastic.py
@@ -7,8 +7,9 @@ class ElasticImporter:
 	def __init__(self,path):
 		self.es = Elasticsearch()
 		self.path = path
-		self.index = "Tweets"
+		self.index = "tweets"
 		self.doc_type = "tweet"
+		self.importTweets()
 
 	def importTweets(self):
 		with open(self.path, "r") as data_file:
@@ -16,20 +17,22 @@ class ElasticImporter:
 				doc = json.loads(line)
 				user_id = doc["user_id"]
 
-				res = es.index(index=self.index,
+				res = self.es.index(index=self.index,
 					doc_type=self.doc_type, body=doc)
 
 				print(res['created'])
 
 	def checkImport(self):
-		es.indices.refresh(index=self.index)
-		res = es.search(index=self.index, body={"query": {"match_all": {}}})
+		self.es.indices.refresh(index=self.index)
+		res = self.es.search(index=self.index, body={"query": {"match_all": {}}})
 		print("Got %d Hits:" % res['hits']['total'])
 		for hit in res['hits']['hits']:
 			print("%(user_id)s %(tweet_id)s: %(text)s" % hit["_source"])
 
-def main(args):
-	print(args)
+def main(path):
+	print("Will import " +path  + " into elasticsearch")
+	importer = ElasticImporter(path)
+	importer.checkImport()
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
